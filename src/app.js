41ea3dd4a136782f1090edf2c0d269e3864020bf
Jethanand Malhi <jethanandj52@gmail.com>
2026-01-04 22:31:15 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ io.on("connection", socket => {
 // Middlewares
 // ==========================
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "https://task-hive-entrovex.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://belog-frontend.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
